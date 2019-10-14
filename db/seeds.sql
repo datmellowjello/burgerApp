6dd1a56c2d4e37255bf547e9bc60819d9155e01c
davidbridges123 <david.bridges@live.com>
2019-10-14 17:09:30 -0600

    updates

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name) VALUES ('Vegan Burger');
+INSERT INTO burgers (burger_name) VALUES ('Steak Burger');
+INSERT INTO burgers (burger_name) VALUES ('Rice Burger');
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('salmon burger');
+
