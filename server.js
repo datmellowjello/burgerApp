ac341ee950d37c9d161445544b1f08c7cfc98b23
davidbridges123 <david.bridges@live.com>
2019-10-23 14:29:22 -0600

    update

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
 var express = require("express");
 var exphbs  = require('express-handlebars');
 var db = require("./models")
+
+var PORT = process.env.PORT || 10001;
+
+var app = express();
+
+app.use(express.static("assets"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+let routes = require("./controllers/burgers_controllers")
+
+app.use(routes);
+
+db.sequelize.sync().then(()=>{
+  app.listen(PORT, function() {
+  console.log("App now listening at http://localhost:" + PORT);
+});})
